ce7b562a0d1cba4982c90225d3a45d9580e2e9cf
Dan Johnson <danimal.johnson@gmail.com>
2023-01-13 23:35:58 -0800

    wip: progress on auth router

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,5 +1,31 @@
-import express from '@prisma/client';
+import express from 'express';
 import type { Request, Response } from 'express';
 
 import * as authService from './auth.service';
 
+export const authRouter = express.Router();
+
+// POST /api/signup
+authRouter.post('/signup', async (req: Request, res: Response) => {
+  try {
+    const user = await authService.signup(req.body);
+    res.json(user);
+  }
+  catch (err: any) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
+// POST /api/login
+authRouter.post('/login', async (req: Request, res: Response) => {
+  try {
+    const user = authService.login(req.body.username, req.body.password);
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid username or password' });
+    }
+    res.json(user);
+  }
+  catch (err: any) {
+    return res.status(500).json({ message: err.message });
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,23 @@
 import { db } from '../utils/db.server';
 import { hashPassword } from '../utils/utils';
 
+const login = async (email: string, password: string) => {
+  const user = await db.users.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new Error('User not found');
+  }
+
+  // TODO: add salt to user. Then: 2nd arg is user.salt
+  const passwordMatch = await hashPassword(password) === user.password;
+
+  if (!passwordMatch) {
+    throw new Error('Password does not match');
+  }
+
+  return user;
+}
